38e393e4a4748946557484f75ab4a525c0e2ea17
tahsin-roza <tahsinroza10@gmail.com>
2026-03-01 22:07:36 +0600

    .......

---
+++ 3.longest_substrg_without_repeat.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        
+        if(s.length() == 0) return 0;
+        
+        unordered_set<char> st;
+        int left = 0;
+        int ans = 0;
+        
+        for(int right = 0; right < s.length(); right++) {
+            
+            while(st.find(s[right]) != st.end()) {
+                st.erase(s[left]);
+                left++;
+            }
+            
+            st.insert(s[right]);
+            ans = max(ans, right - left + 1);
+        }
+        
+        return ans;
+    }
+};
